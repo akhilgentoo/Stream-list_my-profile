a45db69b71df3c4ed0debd91f936fc57302623da
Akhilkumar <akhilkumar8609@gmail.com>
2024-08-13 14:36:37 +0530

    updated the social media links
    updated

--- app.py
+++ app.py
@@ -59,11 +59,10 @@ with col2:
     st.write("📫", EMAIL)
 
 
-# # --- SOCIAL LINKS ---
-# st.write('\n')
-# cols = st.columns(len(SOCIAL_MEDIA))
-# for index, (platform) in enumerate(SOCIAL_MEDIA.items()):
-#     cols[index].write(f"[{platform}]")
+st.write('\n')
+cols = st.columns(len(SOCIAL_MEDIA))
+for index, (platform, link) in enumerate(SOCIAL_MEDIA.items()):
+    cols[index].write(f"[{platform}]({link})")
 
 
 # --- EXPERIENCE & QUALIFICATIONS ---
